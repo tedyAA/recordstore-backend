b3dec1053f628b535512d50b9187bed6c3ff7453
Teodora <tedy.aa@abv.bg>
2022-05-11 22:35:48 +0300

    added delete update user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,21 +2,46 @@ module Api
   module V1
     class UsersController < ApplicationController
       before_action :authorize_access_request!
-
+      before_action :set_user, only: [:show, :update, :destroy]
       # GET /users
       def index
         if current_user.has_role? :admin
           render json: user_includes
         else
-          render json: "Access denied"
+          render json: is_admin
         end
+      end
+
+      def update
+        if @user.update(user_params)
+          render json: @user
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @user.destroy
       end
 
       private
 
       def user_includes
-        { records: current_user.records.all,
-          users: User.all }
+        { artists: Artist.all,
+          users: User.all,
+          isAdmin: (current_user.has_role? :admin) }
+      end
+
+      def is_admin
+        { isAdmin: (current_user.has_role? :admin) }
+      end
+
+      def set_user
+        @user = User.find(params[:id])
+      end
+
+      def user_params
+        params.require(:user).permit(:email)
       end
     end
   end
